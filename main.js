ff6b7e862217ed6edb6cc7c482b14db0f7098cdd
FilbertKimasa <filbertdan67@gmail.com>
2023-07-24 16:05:57 +0300

    Add event listener

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const closeMenu = document.querySelector("#close-menu");
 const mobileNav = document.querySelector("#mobile-nav");
 const navLinkClose = document.querySelectorAll(".nav-link-close");
 
+navOpen.addEventListener("click", () => {
+  mobileNav.style.display = "flex";
+  document.body.style.overflow = "hidden";
+});
+
 const closeMobileNav = () => {
   mobileNav.style.display = "none";
   document.body.style.overflow = "auto";
