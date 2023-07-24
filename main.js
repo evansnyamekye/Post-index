cf26485081d41256764e3d8df0c6209c22e66759
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-07-24 12:58:37 +0000

    Add mobile-menu files

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-const navOpen = document.querySelector('#nav-open');
 const closeMenu = document.querySelector('#close-menu');
 const mobileNav = document.querySelector('#mobile-nav');
 const navLinkClose = document.querySelectorAll('.nav-link-close');
-
-navOpen.addEventListener('click', () => {
-  mobileNav.style.display = 'flex';
-  document.body.style.overflow = 'hidden';
-});
 
 const closeMobileNav = () => {
   mobileNav.style.display = 'none';
